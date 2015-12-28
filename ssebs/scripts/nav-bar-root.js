880d80bb6443af6e5d343e7290d93e7bfd71e3c3
Sebastian Safari <ssebs@ymail.com>
2015-12-28 20:17:03 +0000

    Added /ssebs, for the google search

---
+++ ssebs/scripts/nav-bar-root.js
@@ -0,0 +1,8 @@
+function func() {
+    document.getElementById('insert').innerHTML = "<nav class=\"bg center centerText\" id=\"nav\"><ul><li><a title=\"Breakout Pong\" href=\"games/breakout-pong/index.html\">Breakout Pong</a></li><li><a title=\"A Platformer of Some Sort\" href=\"games/platformer/index.html\">Platformer</a></li><li><a title=\"Sir Flap A Lot\" href=\"games/sir-flap-a-lot/index.html\">Sir Flap Alot</a></li><li><a title=\"Perishable Punchers\" href=\"games/perishable-punchers/index.html\">Perishable Punchers</a></li><li><a title=\"Perishable Punchers V2\" href=\"games/perishable-punchers-v-2/index.html\">Perishable Punchers V.2</a></li></ul></nav>";
+}
+window.onload = func;
+
+/*
+ROOT
+*/
