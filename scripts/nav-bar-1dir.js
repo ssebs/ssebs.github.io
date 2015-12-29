77f2b439e1a6deac3a72684d947553978640d77f
Sebastian Safari <ssebs@ymail.com>
2015-12-29 18:45:54 +0000

    Fixed navbar, added android pages

--- scripts/nav-bar-1dir.js
+++
@@ -1,8 +0,0 @@
-function func() {
-    document.getElementById('insert').innerHTML = "<nav class=\"bg center centerText\" id=\"nav\"><ul><li><a title=\"Breakout Pong\" href=\"breakout-pong/index.html\">Breakout Pong</a></li><li><a title=\"A Platformer of Some Sort\" href=\"platformer/index.html\">Platformer</a></li><li><a title=\"Sir Flap A Lot\" href=\"sir-flap-a-lot/index.html\">Sir Flap Alot</a></li><li><a title=\"Perishable Punchers\" href=\"perishable-punchers/index.html\">Perishable Punchers</a></li><li><a title=\"Perishable Punchers V2\" href=\"perishable-punchers-v-2/index.html\">Perishable Punchers V.2</a></li></ul></nav>";
-}
-window.onload = func;
-
-/*
-1 DIRECTORY DOWN
-*/
